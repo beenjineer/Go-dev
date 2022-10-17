59f9491790a9e67292de17746f3a3df2190ef5ef
Sam Jin <samjin@berkeley.edu>
2022-10-16 23:37:06 -0700

    Added error handling and log functionalities

--- greetings/hello2.go
+++ greetings/hello2.go
@@ -1,15 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 Hello returns a greeting for the named person.
 Hello: Function name, exported
 name string: variable_name parameter_type
-string: return type
+string, error : return type. Returning multiple variables (https://go.dev/doc/effective_go#multiple-returns)
 */
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello3.go
+++ hello/hello3.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/samjin-berkeley/Go-dev/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	/*
+		Set properties of the predefined Logger, including
+		the log entry prefix and a flag to disable printing
+		the time, source file, and line number.
+	*/
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	// If error was returned, print error message and exit program with status code 1
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the message
 	fmt.Println(message)
 }
